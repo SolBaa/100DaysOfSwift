3d2ad8175a218b90a9cb9678b6875cc207ee03d4
Sol <solbattaglia@gmail.com>
2023-01-20 13:48:15 -0300

    Initial Commit

---
+++ first_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  first_project
+//
+//  Created by Sol on 1/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
